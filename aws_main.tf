42614d30c48b3337be3562b15e8614d6025f5705
Kanagaraj A <46182031+Kanagaraj5463@users.noreply.github.com>
2021-03-09 13:10:28 +0530

    Update aws_main.tf

--- aws_main.tf
+++ aws_main.tf
@@ -3,6 +3,7 @@ provider "aws" {
     access_key = "provide the access key"
     secret_key = "provide the secret key"
 }
+
 resource "aws_vpc" "master-vpc"{
 cidr_block= "10.0.0.0/16"
 tags = {
@@ -29,8 +30,8 @@ vpc_id = aws_vpc.master-vpc.id
 cidr_block = "10.0.101.0/24"
 availability_zone = "us-east-1a"
 }
-resource "aws_security_group" "ser" {
-  name        = "ser"
+resource "aws_security_group" "sg-xxxxxxxx" {
+  name        = "sersg-mastersec"
   description = "Allow web traffic"
   vpc_id      = aws_vpc.master-vpc.id
 
@@ -60,13 +61,13 @@ resource "aws_security_group" "ser" {
   }
 
   tags = {
-      Name = "ser"
+      Name = "sg-mastersec"
   }
 }
 resource "aws_network_interface" "ni" {
   subnet_id       = aws_subnet.master-public-subnet.id
   private_ips     = ["10.0.1.24"]
-  security_groups = [aws_security_group.ser.id]
+  security_groups = ["aws_security_group.sg-xxxxxxxx.id"]
   attachment {
     instance     = aws_instance.foobar1.id
     device_index = 1
@@ -74,17 +75,106 @@ resource "aws_network_interface" "ni" {
 }
 resource "aws_eip" "elasticip"{
   vpc      = true
-  network_interface ="aws_network_interface.ni.id"
+  network_interface ="aws_network_interface.id"
   associate_with_private_ip = "10.0.1.24"
   depends_on = [aws_internet_gateway.mastergw]
 }
 resource "aws_launch_template" "foobar" {
-  name_prefix   = "foobar"
-  image_id      = "ami-042e8287309f5df03"
+  block_device_mappings {
+    device_name = "/dev/sda1"
+
+    ebs {
+      volume_size = 20
+    }
+  }
+
+  capacity_reservation_specification {
+    capacity_reservation_preference = "open"
+  }
+
+  cpu_options {
+    core_count       = 4
+    threads_per_core = 2
+  }
+
+  credit_specification {
+    cpu_credits = "standard"
+  }
+
+  disable_api_termination = true
+
+  ebs_optimized = true
+
+  elastic_gpu_specifications {
+    type = "test"
+  }
+
+  elastic_inference_accelerator {
+    type = "eia1.medium"
+  }
+
+  iam_instance_profile {
+    name = "test"
+  }
+
+  image_id = "ami-042e8287309f5df03"
+
+  instance_initiated_shutdown_behavior = "terminate"
+
+  instance_market_options {
+    market_type = "spot"
+  }
+
   instance_type = "t2.micro"
+
+  kernel_id = "test"
+
+  key_name = "test"
+
+  license_specification {
+    license_configuration_arn = "arn:aws:license-manager:us-east-1:123456789012:license-configuration:lic-0123456789abcdef0123456789abcdef"
+  }
+
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 1
+  }
+
+  monitoring {
+    enabled = true
+  }
+
+  network_interfaces {
+    associate_public_ip_address = true
+  }
+
+  placement {
+    availability_zone = "us-east-1"
+  }
+
+  ram_disk_id = "test"
+
+  vpc_security_group_ids = ["sg-xxxxxxxx"]
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "test"
+    }
+  }
+
+  user_data = <<-EOF
+         #! /bin/bash
+         sudo yum install httpd -y
+         sudo systemctl start httpd
+         sudo systemctl enable httpd
+         echo "<h1> sample webserver </h1> >> var /var/www/html/index.html
+  EOF      
 }
 resource "aws_autoscaling_group" "bar" {
-  availability_zones = ["us-east-1a"]
+  availability_zones = ["us-east-1"]
   desired_capacity   = 3
   max_size           = 5
   min_size           = 2
@@ -98,8 +188,9 @@ resource "aws_instance" "foobar1"{
 ami = "ami-042e8287309f5df03"
 instance_type = "t2.micro"
 availability_zone = "us-east-1a"
-security_groups = [aws_security_group.ser.name]
+security_groups = ["aws_security_group.sg-xxxxxxxx.id"]
 key_name = "don"
+associate_public_ip_address = true
 user_data = <<-EOF
          #! /bin/bash
          sudo yum install httpd -y
