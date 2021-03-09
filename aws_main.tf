a5b8c761feb4aedcebc39011bf970917d5a61c31
Kanagaraj A <46182031+Kanagaraj5463@users.noreply.github.com>
2021-03-09 18:59:38 +0530

    Update aws_main.tf

--- aws_main.tf
+++ aws_main.tf
@@ -3,21 +3,10 @@ provider "aws" {
     access_key = "provide the access key"
     secret_key = "provide the secret key"
 }
-
 resource "aws_vpc" "master-vpc"{
 cidr_block= "10.0.0.0/16"
 tags = {
   Environment = "testing"
-}
-}
-resource "aws_internet_gateway" "mastergw"{
-vpc_id = aws_vpc.master-vpc.id
-}
-resource "aws_route_table" "master-route-table"{
-vpc_id = aws_vpc.master-vpc.id
-route {
-  cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.mastergw.id
 }
 }
 resource "aws_subnet" "master-public-subnet"{
@@ -30,26 +19,24 @@ vpc_id = aws_vpc.master-vpc.id
 cidr_block = "10.0.101.0/24"
 availability_zone = "us-east-1a"
 }
-resource "aws_security_group" "sg-xxxxxxxx" {
-  name        = "sersg-mastersec"
-  description = "Allow web traffic"
-  vpc_id      = aws_vpc.master-vpc.id
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
+#create security group
+resource "aws_security_group" "ssh-terra-terra"{
+    name = "hss"
+    ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
+   ingress {
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   ingress {
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -60,25 +47,28 @@ resource "aws_security_group" "sg-xxxxxxxx" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+}
+
+#create an instance
+
+resource "aws_instance" "da1"{
+ami = "ami-042e8287309f5df03"
+instance_type = "t2.micro"
+availability_zone = "us-east-1a"
+security_groups = ["${aws_security_group.ssh-terra-terra.name}"]
+key_name = "don"
+user_data = <<-EOF
+         #! /bin/bash
+         sudo yum install httpd -y
+         sudo systemctl start httpd
+         sudo systemctl enable httpd
+         echo "<h1> sample webserver </h1> >> var /var/www/html/index.html
+  EOF     
   tags = {
-      Name = "sg-mastersec"
-  }
+      Name = "web server"
+  }  
 }
-resource "aws_network_interface" "ni" {
-  subnet_id       = aws_subnet.master-public-subnet.id
-  private_ips     = ["10.0.1.24"]
-  security_groups = ["aws_security_group.sg-xxxxxxxx.id"]
-  attachment {
-    instance     = aws_instance.foobar1.id
-    device_index = 1
-  }
-}
-resource "aws_eip" "elasticip"{
-  vpc      = true
-  network_interface ="aws_network_interface.id"
-  associate_with_private_ip = "10.0.1.24"
-  depends_on = [aws_internet_gateway.mastergw]
-}
+
 resource "aws_launch_template" "foobar" {
   block_device_mappings {
     device_name = "/dev/sda1"
@@ -105,17 +95,6 @@ resource "aws_launch_template" "foobar" {
 
   ebs_optimized = true
 
-  elastic_gpu_specifications {
-    type = "test"
-  }
-
-  elastic_inference_accelerator {
-    type = "eia1.medium"
-  }
-
-  iam_instance_profile {
-    name = "test"
-  }
 
   image_id = "ami-042e8287309f5df03"
 
@@ -126,10 +105,6 @@ resource "aws_launch_template" "foobar" {
   }
 
   instance_type = "t2.micro"
-
-  kernel_id = "test"
-
-  key_name = "test"
 
   license_specification {
     license_configuration_arn = "arn:aws:license-manager:us-east-1:123456789012:license-configuration:lic-0123456789abcdef0123456789abcdef"
@@ -152,17 +127,8 @@ resource "aws_launch_template" "foobar" {
   placement {
     availability_zone = "us-east-1"
   }
-
-  ram_disk_id = "test"
-
-  vpc_security_group_ids = ["sg-xxxxxxxx"]
-
   tag_specifications {
     resource_type = "instance"
-
-    tags = {
-      Name = "test"
-    }
   }
 
   user_data = <<-EOF
@@ -182,21 +148,4 @@ resource "aws_autoscaling_group" "bar" {
     id      = aws_launch_template.foobar.id
     version = "$Latest"
   }
-}
-#create an EC2 instance and start the web server 
-resource "aws_instance" "foobar1"{
-ami = "ami-042e8287309f5df03"
-instance_type = "t2.micro"
-availability_zone = "us-east-1a"
-security_groups = ["aws_security_group.sg-xxxxxxxx.id"]
-key_name = "don"
-associate_public_ip_address = true
-user_data = <<-EOF
-         #! /bin/bash
-         sudo yum install httpd -y
-         sudo systemctl start httpd
-         sudo systemctl enable httpd
-         echo "<h1> sample webserver </h1> >> var /var/www/html/index.html
-  EOF       
-
 }
